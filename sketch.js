e7998c9da75bc2a84ac965bfbd24733c368700f9
vyrwn <byronmardas@gmail.com>
2019-10-19 22:18:37 +0100

    Added icons for the buttons. Working on the import image bags.

--- sketch.js
+++ sketch.js
@@ -48,7 +48,15 @@ var isDrawingJustStarted = false;
 *    DRAWING CANVAS    *
 ************************/
 new p5(function(p) {
+  let img;
   
+  document.getElementById("myFileInput").onclick = function () { LOAD() };
+
+  function LOAD() {
+    img = p.loadImage('test.jpg');
+    p.image(img, 0, 0);
+  }
+
   p.setup = function() {
     
     // Filters used to smooth position and pressure jitter
@@ -129,6 +137,8 @@ new p5(function(p) {
       p.clear();
     }
     
+    
+
   }
 }, "p5_instance_01");
 
@@ -148,22 +158,8 @@ new p5(function(p) {
       
       uiCanvas.clear();
       
-      if(showDebug){
-        // p.text("pressure = " + pressure, 10, 20);
-        
-        p.stroke(200,50);
-        p.line(p.mouseX,0,p.mouseX,p.height);
-        p.line(0,p.mouseY,p.width,p.mouseY);
-
-        // The "loading bar" at the top
-        // is only there as a visual indicator
-        // that the sketch is running
-        p.noStroke();
-        p.fill(100)
-        p.rect(0, 0, p.frameCount % p.width, 4);
-      }
     }
-  	
+    
 
 }, "p5_instance_02");
 
